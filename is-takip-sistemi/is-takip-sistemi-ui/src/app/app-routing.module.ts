74164c8fc11ae27c0050f00ad6839692f5ab548d
OnurAcanGithub <onur.acan@hotmail.com>
2022-12-08 23:44:23 +0300

    Api ve Ui de login sayfası tasarımı oluşturuldu.

--- is-takip-sistemi/is-takip-sistemi-ui/src/app/app-routing.module.ts
+++ is-takip-sistemi/is-takip-sistemi-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SecurityComponent } from './security/security.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'giris', component:SecurityComponent},
+  {path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path:'dashboard', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
