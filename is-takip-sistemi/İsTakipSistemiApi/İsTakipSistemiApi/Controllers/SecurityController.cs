6eb2d03e86117059aa9cdc441c529ecce6820456
OnurAcanGithub <onur.acan@hotmail.com>
2022-12-09 00:10:58 +0300

    .

--- is-takip-sistemi/İsTakipSistemiApi/İsTakipSistemiApi/Controllers/SecurityController.cs
+++ is-takip-sistemi/İsTakipSistemiApi/İsTakipSistemiApi/Controllers/SecurityController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IsTakipSistemiApi.Controllers;
+namespace IsTakipSistemi.API.Controllers;
 
 [ApiController]
 [Route("api/security")]
